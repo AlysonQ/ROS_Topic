8cf7c884d0cca406d2df8ea23430e14af536533a
AlysonQ <qoo810823@gmail.com>
2015-10-03 15:37:56 +0800

    .

--- src/talker.cpp
+++ src/talker.cpp
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "[Cool_Alyson] " << count;
+    ss << "[TALKER] " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
